a3044a6e3b32105f8811467a844cb762ab4488ad
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2023-04-08 19:34:33 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/pages/item/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemPage = () => {
+  return <div>왜 그렇대</div>;
+};
+
+export default ItemPage;
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+  };
+};
