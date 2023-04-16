e43c9bc090723f22985fe55e6b489ce0dfda271d
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2023-04-16 16:40:35 +0900

    feat: OpenAPI api 관련 기본 기능 구현

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import OpenAiInstance from '@/shared/api/openApi';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Response = {
+  text: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+  try {
+    const response = await OpenAiInstance.requestPrompt(req.body);
+    res.status(200).json({ text: response.data.choices[0].text || 'error' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+}
---
+++ src/shared/api/openApi.ts
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+class OpenAI {
+  private openAI: OpenAIApi | null;
+
+  constructor() {
+    this.openAI = null;
+  }
+
+  setInstance(apiKey: string) {
+    const configuration = new Configuration({
+      organization: 'org-w9QQmOR8mQAdGCSx6Wdg664e',
+      apiKey: apiKey,
+    });
+
+    this.openAI = new OpenAIApi(configuration);
+  }
+
+  requestPrompt(prompt: string) {
+    if (!this.openAI) {
+      throw new Error('Please set OpenAPI API key');
+    }
+
+    return this.openAI.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+  }
+}
+
+const OpenAiInstance = new OpenAI();
+
+export default OpenAiInstance;
